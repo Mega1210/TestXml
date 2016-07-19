ce771f59bda48f76cbb4b1550664ec6c74a06927
Mega <mega73@gmail.com>
2016-07-19 14:37:08 +0100

    reads XML!!!

--- TestXml/Form1.Designer.cs
+++ TestXml/Form1.Designer.cs
@@ -28,12 +28,50 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.gv1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.gv1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(22, 237);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(414, 137);
+            this.textBox1.TabIndex = 1;
+            // 
+            // gv1
+            // 
+            this.gv1.AllowUserToAddRows = false;
+            this.gv1.AllowUserToDeleteRows = false;
+            this.gv1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gv1.Location = new System.Drawing.Point(33, 12);
+            this.gv1.Name = "gv1";
+            this.gv1.ReadOnly = true;
+            this.gv1.RowTemplate.Height = 24;
+            this.gv1.Size = new System.Drawing.Size(357, 195);
+            this.gv1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(487, 386);
+            this.Controls.Add(this.gv1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.gv1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataGridView gv1;
     }
 }
 
--- TestXml/Form1.cs
+++ TestXml/Form1.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Linq;
+using System.Xml;
+
 
 namespace TestXml
 {
@@ -15,6 +18,69 @@ namespace TestXml
         public Form1()
         {
             InitializeComponent();
+           
+
+            DataTable dt = new DataTable("Table");
+
+            /* Region, Salesperson Code, Salesperson, Customer, Eq Cases This Month, 
+             * Eq Cases This Month LY, Eq Cases % Achieved, Amount This Month, Amount This Month LY, Amount % Achieved, 
+             * GM This Month, GM This Month LY, GM % Achieved */
+            dt.Columns.Add("Region", typeof(System.String));
+            dt.Columns.Add("Salesperson_Code", typeof(System.String));
+            dt.Columns.Add("Salesperson", typeof(System.String));
+            dt.Columns.Add("Customer_ID", typeof(System.String));
+            dt.Columns.Add("Eq_Cases_This_Month", typeof(System.String));
+            dt.Columns.Add("Eq_Cases_This_Month_LY", typeof(System.String));
+            dt.Columns.Add("EqCasesperc", typeof(System.String));
+            dt.Columns.Add("Amount_This_Month", typeof(System.String));
+            dt.Columns.Add("Amount_This_Month_LY", typeof(System.String));
+            dt.Columns.Add("Amountperc", typeof(System.String));
+            dt.Columns.Add("GM_This_Month", typeof(System.String));
+            dt.Columns.Add("GM_This_Month_LY", typeof(System.String));
+            dt.Columns.Add("GMperc", typeof(System.String));
+
+            string fileName = @"\\ewcweb\reports\CustomerSalesRep.xml";
+
+            XmlDocument doc = new XmlDocument();
+
+            doc.Load(fileName);
+
+            //XmlNode root = doc.SelectSingleNode("/");
+            XmlNode root = doc.FirstChild.NextSibling.FirstChild.FirstChild;
+
+            XmlNodeList nodes = root.ChildNodes;
+            
+        
+                for (int ii = 0; ii < nodes.Count; ii++)
+                {
+                for (int i=0;i<13;i++)
+                {
+                dt.Rows.Add();
+                    dt.Rows[ii][i]=nodes.Item(ii).ChildNodes.Item(i).InnerText;
+
+                }
+                    /*dt.Rows[ii]["Salesperson_Code"] = nodes.Item(ii).ChildNodes.Item(1).InnerText;
+                    dt.Rows[ii]["Salesperson"] = nodes.Item(ii).ChildNodes.Item(2).InnerText;
+                    dt.Rows[ii]["Customer_ID"] = nodes.Item(ii).ChildNodes.Item(3).InnerText;
+                    dt.Rows[ii]["Eq_Cases_This_Month"] = nodes.Item(ii).ChildNodes.Item(4).InnerText;
+                    dt.Rows[ii]["Eq_Cases_This_Month_LY"] = nodes.Item(ii).ChildNodes.Item(5).InnerText;
+                    dt.Rows[ii]["EqCasesperc"] = nodes.Item(ii).ChildNodes.Item(6).InnerText;
+                    dt.Rows[ii]["Amount_This_Month"] = nodes.Item(ii).ChildNodes.Item(7).InnerText;
+                    dt.Rows[ii]["Amount_This_Month_LY"] = nodes.Item(ii).ChildNodes.Item(8).InnerText;
+                    dt.Rows[ii]["Amountperc"] = nodes.Item(ii).ChildNodes.Item(9).InnerText;
+                    dt.Rows[ii]["GM_This_Month"] = nodes.Item(ii).ChildNodes.Item(10).InnerText;
+                    dt.Rows[ii]["GM_This_Month_LY"] = nodes.Item(ii).ChildNodes.Item(11).InnerText;
+                    dt.Rows[ii]["GMperc"] = nodes.Item(ii).ChildNodes.Item(12).InnerText;*/
+                }
+         
+
+
+            gv1.DataSource = dt;
+            
+            
+
+            
+
         }
     }
 }
